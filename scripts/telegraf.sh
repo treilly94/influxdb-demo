f4fa94eb77aed80ef95f6b34f65c3b3c54b87035
tom reilly <treilly94@live.com>
2020-01-18 14:17:58 +0000

    added tick specific config

--- scripts/telegraf.sh
+++ scripts/telegraf.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-sudo mkdir /etc/telegraf/
-
 sudo cp /vagrant/config/telegraf.conf /etc/telegraf/telegraf.conf
+
+if [ $HOSTNAME = "influxdb" ]
+then
+	sudo cp /vagrant/config/telegraf_tick.conf /etc/telegraf/telegraf.d/telegraf_tick.conf
+fi
 
 curl -o telegraf.rpm https://dl.influxdata.com/telegraf/releases/telegraf-1.13.1-1.x86_64.rpm
 sudo yum localinstall -y telegraf.rpm
